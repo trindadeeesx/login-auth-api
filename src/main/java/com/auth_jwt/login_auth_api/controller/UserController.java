67cd5048d33007a8f51aa07eb6792b819dee09c3
trindadeeesx <trindadesx@proton.me>
2025-11-15 18:46:51 -0300

    feat: implement user retrieval in UserController

--- src/main/java/com/auth_jwt/login_auth_api/controller/UserController.java
+++ src/main/java/com/auth_jwt/login_auth_api/controller/UserController.java
@@ -1,16 +1,25 @@
 package com.auth_jwt.login_auth_api.controller;
 
+import com.auth_jwt.login_auth_api.domain.user.User;
+import com.auth_jwt.login_auth_api.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+	@Autowired
+	private UserRepo userRepo;
+
 	@GetMapping
-	public ResponseEntity<String> getUsers() {
-		return ResponseEntity.ok("sucesso");
+	public ResponseEntity<List<User>> getUsers() {
+		List<User> users = userRepo.findAll();
+		return ResponseEntity.ok(users);
 	}
 }
