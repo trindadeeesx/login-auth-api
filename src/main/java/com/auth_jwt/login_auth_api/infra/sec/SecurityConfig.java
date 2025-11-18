e577e017f963e9c9d8b75650acfec27d1aa8b663
trindadeeesx <trindadesx@proton.me>
2025-11-18 14:18:46 -0300

    :no_entry_sign: fix: adjust CORS configuration

--- src/main/java/com/auth_jwt/login_auth_api/infra/sec/SecurityConfig.java
+++ src/main/java/com/auth_jwt/login_auth_api/infra/sec/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+				.cors(cors -> {})
 				.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(authorize -> authorize
