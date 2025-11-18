552b9cb1c8530b205e32fa6b6bab25ae85052530
trindadeeesx <trindadesx@proton.me>
2025-11-18 14:13:01 -0300

    :link: feat: changed cors origin to 5173 (vite) and allow headers

--- src/main/java/com/auth_jwt/login_auth_api/infra/cors/CorsConfig.java
+++ src/main/java/com/auth_jwt/login_auth_api/infra/cors/CorsConfig.java
@@ -11,10 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "DELETE", "PUT");
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:5173")
+				.allowedMethods("GET", "POST", "DELETE", "PUT")
+				.allowedHeaders("*")
+				.allowCredentials(true);
+	}
 }
